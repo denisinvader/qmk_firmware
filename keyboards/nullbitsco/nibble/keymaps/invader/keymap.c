10232236a538f27687cea341c2e127d1e3c43d95
Mikhail Panichev <denis.invader@gmail.com>
2021-04-14 23:00:19 +0300

    keymaps

--- keyboards/nullbitsco/nibble/keymaps/invader/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/invader/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ENC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_NUBS,
         KC_F12, LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_HOME,
         _______, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,  KC_END,
-        _______, KC_LCTL, KC_LALT, KC_LGUI,       KC_SPC,      MO(_FN), KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        _______, KC_LCTL, KC_LALT, KC_LGUI,       KC_SPC,      MO(_FN), KC_RGUI, MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FN] = LAYOUT_ansi(
                 KC_NUHS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_INS,
